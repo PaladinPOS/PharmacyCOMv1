96012dcf1eb0ae2d7d1340736f5853094faf7196
bradp198 <78925051+bradp198@users.noreply.github.com>
2021-02-15 12:56:39 -0800

    Add files via upload

---
+++ PaladinPharmacyCOMv1.Encryption/PharmacyCOMEncryption.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PaladinPharmacyCOMv1.Encryption
+{
+    class PharmacyCOMEncryption
+    {
+        public PharmacyCOMEncryption(string pwd)
+        {
+            var hash = MD5.Create();
+            this.key = hash.ComputeHash(Encoding.Unicode.GetBytes(pwd ?? string.Empty));
+        }
+
+        private byte[] key;
+
+        public string Encrypt(string msg)
+        {
+            try
+            {
+                var aes = Aes.Create();
+                aes.Key = key;
+                aes.GenerateIV();
+
+                var bytes = Encoding.Unicode.GetBytes(msg);
+                byte[] encrypted;
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytes, 0, bytes.Length);
+                    }
+                    encrypted = ms.ToArray();
+                }
+
+                //return iv prepended to data
+                var totalBytes = new byte[encrypted.Length + aes.IV.Length];
+                Buffer.BlockCopy(aes.IV, 0, totalBytes, 0, aes.IV.Length);
+                Buffer.BlockCopy(encrypted, 0, totalBytes, aes.IV.Length, encrypted.Length);
+                return Convert.ToBase64String(totalBytes);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public string Decrypt(string strEncrypted)
+        {
+            try
+            {
+                var encrypted = Convert.FromBase64String(strEncrypted);
+
+                var aes = Aes.Create();
+                aes.Key = key;
+
+                //extract iv from byte array
+                aes.IV = encrypted.Take(16).ToArray();
+                encrypted = encrypted.Skip(16).ToArray();
+
+                var decrypted = new byte[encrypted.Length];
+
+                using (var ms = new MemoryStream(encrypted))
+                {
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        cs.Read(decrypted, 0, decrypted.Length);
+                    }
+                }
+
+                return Encoding.Unicode.GetString(decrypted);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+
+        }
+    }
+}
---
+++ PaladinPharmacyCOMv1.Encryption/PharmacyCOMSoapExtension.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Web.Services.Protocols;
+
+namespace PaladinPharmacyCOMv1.Encryption
+{
+    class PharmacyCOMSoapExtension : SoapExtension
+    {
+        private Stream inStream;
+        private Stream outStream;
+
+        public override Stream ChainStream(Stream stream)
+        {
+            outStream = stream;
+            inStream = new MemoryStream();
+            return inStream;
+        }
+        public override object GetInitializer(Type serviceType)
+        {
+            return null;
+        }
+        public override object GetInitializer(LogicalMethodInfo methodInfo, SoapExtensionAttribute attribute)
+        {
+            return null;
+        }
+        public override void Initialize(object initializer)
+        {
+            return;
+        }
+        public override void ProcessMessage(SoapMessage message)
+        {
+            string soapMsg;
+            StreamReader sRdr;
+            StreamWriter sWrt;
+
+            bool encryptionEnabled = false; //read from settings file
+            string password = "PalPharmCom"; //read from settings file
+            var encryption = new PharmacyCOMEncryption(password);
+
+            switch (message.Stage)
+            {
+                case SoapMessageStage.BeforeDeserialize:
+
+                    sRdr = new StreamReader(outStream);
+                    sWrt = new StreamWriter(inStream);
+
+                    if (encryptionEnabled)
+                    {
+                        soapMsg = encryption.Decrypt(sRdr.ReadToEnd());
+                    }
+                    else
+                    {
+                        soapMsg = sRdr.ReadToEnd();
+                    }
+
+                    sWrt.Write(soapMsg);
+                    sWrt.Flush();
+
+                    inStream.Position = 0;
+                    
+                    break;
+
+                case SoapMessageStage.AfterSerialize:
+
+                    inStream.Position = 0;
+
+                    sRdr = new StreamReader(inStream);
+                    sWrt = new StreamWriter(outStream);
+
+                    if (encryptionEnabled)
+                    {
+                        soapMsg = encryption.Encrypt(sRdr.ReadToEnd());
+                    }
+                    else
+                    {
+                        soapMsg = sRdr.ReadToEnd();
+                    }
+
+                    sWrt.Write(soapMsg);
+                    sWrt.Flush();
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
